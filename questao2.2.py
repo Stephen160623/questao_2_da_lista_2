f5fb493f380754ab5920c53712f30518923c041c
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-30 16:49:14 -0300

    Add files via upload

---
+++ questao2.2.py
@@ -0,0 +1,22 @@
+# Atribuição de variáveis
+   
+# Entrada de dados
+
+x = float(input("Digite um número: "))
+
+#Processamento de dados
+
+y = x%2
+z = x%1
+
+# Saída de dados
+
+if x<0:
+    print("Número inválido")
+elif z != 0:
+    print("Número inválido")
+else:
+    if y == 0:
+        print("Número é par")
+    else:
+        print("Número é ímpar")
